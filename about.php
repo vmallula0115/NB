45f21a3d5ff4e67ee68c1f1f0692964410cd268e
vmallula0115 <nani.mallula777@gmail.com>
2022-09-17 01:58:42 +0530

    One

---
+++ about.php
@@ -0,0 +1,5 @@
+<h2>About Us</h2>
+<h2>E-Notice Board allows the students to view their time table or information regarding their
+respective branch or course . It is a web based application which is accessible by the every
+student with their individual registration id&#39;s and password. It carries with all the information
+and updates from time-to-time</h3>
